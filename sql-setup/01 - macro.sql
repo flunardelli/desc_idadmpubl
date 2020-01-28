303fcbc6aa55979f062a9a50b2803e9e6a41d681
usuario <robsoneducardo@gmail.com>
2020-01-28 10:20:10 -0300

    sql de inserção no banco de dados MySQL dos dados geográficos: regiões, estados e municípios de acordo com o IBGE.

---
+++ sql-setup/01 - macro.sql
@@ -0,0 +1,5 @@
+insert into macrorregiao (cod, nome) values (1,'Norte');
+insert into macrorregiao (cod, nome) values (2,'Nordeste');
+insert into macrorregiao (cod, nome) values (3,'Sudeste');
+insert into macrorregiao (cod, nome) values (4,'Sul');
+insert into macrorregiao (cod, nome) values (5,'Centro-oeste');
